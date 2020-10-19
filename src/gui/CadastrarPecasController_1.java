1274a0d5ee6f551dc23e7cc3db8a131c9def55af
Michelle Silva <michellelicia98@gmail.com>
2020-10-18 23:04:45 -0300

    Adicionando tela Atualizar Peças

---
+++ src/gui/CadastrarPecasController_1.java
@@ -0,0 +1,56 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author Gedenilton
+ */
+public class CadastrarPecasController implements Initializable {
+
+	@FXML // fx:id="TextFieldCodigoPeca"
+	private TextField TextFieldCodigoPeca;
+	@FXML // fx:id="TextFieldDescricaoPeca"
+	private TextField TextFieldDescricaoPeca;
+
+	@FXML // fx:id="TextFieldPreçoPeca"
+	private TextField TextFieldPreçoPeca;
+
+	@FXML // fx:id="TextFieldQtdEstoquePeca"
+	private TextField TextFieldQtdEstoquePeca;
+
+	@FXML // fx:id="btnCancelar"
+	private Button btnCancelar;
+
+	@FXML // fx:id="btnOK"
+	private Button btnOK;
+
+	@FXML
+	void opcoesPecas(ActionEvent event) {
+
+	}
+	
+	/*
+	 * Ao apertar o botão cancelar o codigo deve voltar para a TelaColaborador
+	 * falta consertar essa funcionalidade
+	 * */
+	@FXML
+    void Cancelar(ActionEvent e) {
+    	System.out.println("testando no console!");
+    	JavaFXApplication.MudarTela("telaPrincipal");
+    	    	
+    }
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+
+	}
+
+}
--- src/gui/JavaFXApplication.java
+++ src/gui/JavaFXApplication.java
@@ -20,6 +20,7 @@ public class JavaFXApplication extends Application {
 	private static Scene telaPrincipal;
 	private static Scene telaListarPecas;
 	private static Scene telaCadastrarPecas;
+        private static Scene telaAtualizarPecas;
 
 	@Override
 	public void start(Stage stage) throws Exception {
@@ -36,13 +37,17 @@ public class JavaFXApplication extends Application {
 		Parent fxmlPrincipal = FXMLLoader.load(getClass().getResource("TelaColaborador.fxml"));
 		telaPrincipal = new Scene(fxmlPrincipal);
 
-		//Código para carregar a tela ListarPecas
+		//Cï¿½digo para carregar a tela ListarPecas
 		Parent fxmlListarPecas = FXMLLoader.load(getClass().getResource("TelaListarPecas.fxml"));
 		telaListarPecas = new Scene(fxmlListarPecas);
 		
-		//Código para carregar a tela CadastrarPecas
+		//Cï¿½digo para carregar a tela CadastrarPecas
 		Parent fxmlCadastrarPecas = FXMLLoader.load(getClass().getResource("TelaCadastrarPecas.fxml"));
 		telaCadastrarPecas = new Scene(fxmlCadastrarPecas);
+                
+                //Cï¿½digo para carregar a tela AtualizarPecas
+		Parent fxmlAtualizarPecas = FXMLLoader.load(getClass().getResource("TelaAtualizarPecas.fxml"));
+		telaAtualizarPecas = new Scene(fxmlAtualizarPecas);
 
 		//Scene scene = new Scene(fxmlPrincipal);
 		stage.setScene(telaPrincipal);
@@ -51,18 +56,18 @@ public class JavaFXApplication extends Application {
 	
 	public static void MudarTela(String opcoes) {
 		switch(opcoes) {
-		case "Listar peças":
+		case "Listar peï¿½as":
 			palco.setScene(telaListarPecas);
 			break;
 			
-		case "Cadastrar peças":
+		case "Cadastrar peï¿½as":
 			palco.setScene(telaCadastrarPecas);
 			break;
 		
-		case "Atualizar peças":
+		case "Atualizar peï¿½as":
 			palco.setScene(telaListarPecas);
 			break;
-		case "Excluir peças":
+		case "Excluir peï¿½as":
 			palco.setScene(telaListarPecas);
 			break;
 		case " ":
